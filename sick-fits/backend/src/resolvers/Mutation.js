118ce82d206cf67346918a56e5efdd46be1af41a
Dzianis Makeichyk <dzianis.makeichyk@gmail.com>
2018-11-29 15:44:44 +0100

    23

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         const item = await ctx.db.mutation.createItem({
@@ -28,9 +31,31 @@ const Mutations = {
         // Find the item
         const item = await ctx.db.query.item({ where }, `{ id title }`);
         // Check if the own that item, or have the permission
-
+        // TODO
         // Delete it
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+
+    async signup(parent, args, ctx, info) {
+        // lowercase their email
+        args.email = args.email.toLowerCase();
+        // has their password
+        args.password = 'dogs123';
+        const password = await bcrypt.hash(args.password, 10);
+        // create a users db
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permission: { set: ['USER'] }
+            }
+        }, info);
+        // create the JWT token from them
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+        // we set the jws as a cookie on the response
+        ctx.response.cookie('token', token, {
+            http
+        })
     }
 };
 

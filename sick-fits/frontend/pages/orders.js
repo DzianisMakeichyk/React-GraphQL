35d62e436db9c5435fa8b6d2e3e89a8942081604
Dzianis Makeichyk <dzianis.makeichyk@gmail.com>
2018-12-18 13:05:43 +0100

    53

---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,13 @@
+import PleaseSignIn from '../components/PleaseSignIn'
+import OrderList from '../components/OrderList'
+
+const Orders = props => (
+    <div>
+        <PleaseSignIn>
+            <OrderList />
+        </PleaseSignIn>
+
+    </div>
+);
+
+export default Orders;
